a0a36e0c21d37b2d81c032f7b9fb7a1527b6d935
Amir Barylko <amir@barylko.com>
2011-06-30 12:51:17 -0500

    Added test to add a movie

--- lib/medialibrary/library.rb
+++ lib/medialibrary/library.rb
@@ -1,6 +1,8 @@
 module MediaLibrary
   
   class Library
+    
+    
   end
   
 end
--- spec/medialibrary/library_spec.rb
+++ spec/medialibrary/library_spec.rb
@@ -8,6 +8,13 @@ module MediaLibrary
       library = Library.new
     end
     
+    it "should add a movie to the library" do
+      library = Library.new
+      m1 = Movie.new('Blazing Saddles')
+      library.add m1
+      library.contents.should == [m1]
+    end
+    
   end
   
 end
