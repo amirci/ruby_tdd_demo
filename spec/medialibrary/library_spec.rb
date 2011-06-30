31c642cff9a088610404c8912dd3a51476e5ab8a
Amir Barylko <amir@barylko.com>
2011-06-30 14:58:16 -0500

    A bit of refactoring, however the test still smells

--- spec/medialibrary/library_spec.rb
+++ spec/medialibrary/library_spec.rb
@@ -4,32 +4,25 @@ module MediaLibrary
 
   describe Library do
   
-    it "should create a library" do
-      library = Library.new
+    before(:each) do
+      @library = Library.new
+      @expected = Array.new(10) { |i| Movie.new("New movie #{i}") }
     end
     
     it "should add a movie to the library" do
-      # arrange
-      library = Library.new
-      expected = Array.new(10) { |i| Movie.new("New movie #{i}") }
-      
       # act
-      expected.each { |m| library.add(m) }
+      @expected.each { |m| @library.add(m) }
       
       # assert
-      library.contents.should == expected
+      @library.contents.should == @expected
     end
 
     it "should return top 10 movies" do
-      #arrange
-      library = Library.new
-      expected = Array.new(10) { |i| Movie.new("New movie #{i}") }
-      
       # act
-      expected.each { |m| library.add(m) }
+      @expected.each { |m| @library.add(m) }
 
       # act & assert
-      library.top_10.should == expected
+      @library.top_10.should == @expected
     end
     
   end
